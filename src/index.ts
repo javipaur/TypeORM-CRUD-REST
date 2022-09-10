afc3517b19cb08b5de056ac2b64f33cbb1993c4a
Javipaur <javipaur@gmail.com>
2022-09-10 11:08:06 +0200

    db conection & config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+import app from "./app";
+import {AppDataSource} from "./db";
 
-const app =express();
 
-app.listen(3000);
-app.use(morgan('dev'));
-app.use(cors());
-console.log('server is listening on port',3000)
+async function main(){
+    try{
+        await AppDataSource.initialize();
+        console.log('database conected');
+        app.listen(3000);
+        console.log('server is listening on port',3000);
+    }catch(error){
+        console.log(error);
+        
+    }
+   
+}
+
+main();
+
+
+
+
 
